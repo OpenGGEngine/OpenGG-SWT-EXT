2f2f24c08e977a6fd4e9b158d6bc6bc81ab9597e
javster101 <javster101@outlook.com>
2017-07-15 21:52:39 -0400

    Fixed KeyboardHandler

--- OpenGG SWT/src/com/opengg/module/swt/input/SWTKeyboardHandler.java
+++ OpenGG SWT/src/com/opengg/module/swt/input/SWTKeyboardHandler.java
@@ -6,8 +6,10 @@
 
 package com.opengg.module.swt.input;
 
+import static com.opengg.core.io.input.keyboard.Key.*;
 import com.opengg.core.io.input.keyboard.KeyboardController;
 import com.opengg.core.io.input.keyboard.KeyboardHandler;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.KeyEvent;
 import org.eclipse.swt.events.KeyListener;
 
@@ -19,19 +21,50 @@ public class SWTKeyboardHandler extends KeyboardHandler implements KeyListener{
 
     @Override
     public void keyPressed(KeyEvent e) {
-
-        if(e.keyCode < 65535 && keys[e.keyCode] != true){
-            KeyboardController.keyPressed(Character.toUpperCase(e.keyCode));
+        int nkey = SWTToGLFW(e.keyCode);
+        if(nkey >= 65535){
+            return;
         }
-        if(e.keyCode < 65535)
-            keys[e.keyCode] = true;
+        if(keys[nkey] == false){
+            KeyboardController.keyPressed(nkey); 
+            keys[nkey] = true;
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        if(e.keyCode < 65535)
-            keys[e.keyCode] = false;
-        KeyboardController.keyReleased(Character.toUpperCase(e.keyCode));
+        int nkey = SWTToGLFW(e.keyCode);
+        if(nkey >= 65535)
+            return;
+        if(keys[nkey] == true){
+            KeyboardController.keyReleased(nkey); 
+            keys[nkey] = false;
+        }
     }
     
+    public int SWTToGLFW(int key){
+        if(key == SWT.SHIFT)
+            return KEY_LEFT_SHIFT;
+        else if(key == SWT.CONTROL)
+            return KEY_LEFT_CONTROL;
+        else if(key == SWT.ARROW_UP)
+            return KEY_UP;
+        else if(key == SWT.ARROW_DOWN)
+            return KEY_DOWN;
+        else if(key == SWT.ARROW_LEFT)
+            return KEY_LEFT;
+        else if(key == SWT.ARROW_RIGHT)
+            return KEY_RIGHT;
+        else if(key == SWT.ALT)
+            return KEY_LEFT_ALT;
+        else if(key == SWT.TAB)
+            return KEY_TAB;
+        else if(key == SWT.BS)
+            return KEY_BACKSPACE;
+        else if(key == SWT.DEL)
+            return KEY_DELETE;
+        else if(Character.isLetter(key))
+            return Character.toUpperCase(key);
+        return key;
+    }
 }
